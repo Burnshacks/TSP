5cf336892f69254a71f87b3284f76758df5a3dc1
Jan Beckschewe <jan.becksy@gmail.com>
2017-11-09 21:52:47 +0100

    make code look better

--- TSP Bruteforce/StadtApp.java
+++ TSP Bruteforce/StadtApp.java
@@ -1,25 +1,18 @@
-
 import java.awt.*;
 import javax.swing.*;
 
 
-public class StadtApp extends JApplet
-{
-   StadtArray altkreis;
-   
-    public void init()
-    {
-     altkreis = new StadtArray();
-     //altkreis.erzeuge();
-     //altkreis.nearestNeighbour();
-     //altkreis.entfernungsliste();
+public class StadtApp extends JApplet {
+    private StadtArray randomCities;
+
+    public void init() {
+        randomCities = new StadtArray();
     }
 
-    
-    public void paint(Graphics g)
-    {
-        altkreis.paint(g);
+
+    public void paint(Graphics g) {
+        randomCities.paint(g);
     }
 
-    
+
 }
--- TSP Bruteforce/StadtArray.java
+++ TSP Bruteforce/StadtArray.java
@@ -1,126 +1,103 @@
 import java.awt.*;
-import java.util.*;
-public class StadtArray
-{
-    ArrayList<Stadt> lst;
-    int[][] entfernung;
-    int anzahl;
-    int gesamtlaenge = 6000;
-    public ArrayList<Stadt> bestRoute;
-    public ArrayList<Stadt> shortestRoute;
-    Random rand;
-    Stadt stadt;
-    
-    public StadtArray()
-    {
-        anzahl =Stadt.anzahl;
-        ArrayList<Stadt> lst = new ArrayList<Stadt>();
-        for (int i = 0; i < anzahl; ++i){
-            
-            lst.add(new Stadt(i,getRandomInt(),getRandomInt(),"Stadt "+(i)));
+import java.util.ArrayList;
+import java.util.Random;
+
+class StadtArray {
+    private int[][] entfernungen;
+    private int anzahl = 10;
+    private int gesamtlaenge = 6000;
+    private ArrayList<Stadt> possibleRoutes;
+    private ArrayList<Stadt> shortestRoute;
+    private Random rand = new Random();
+
+    StadtArray() {
+        ArrayList<Stadt> lst = new ArrayList<>();
+        for (int i = 0; i < anzahl; ++i) {
+
+            lst.add(new Stadt(rand.nextInt(800), rand.nextInt(800), "Stadt " + (i)));
         }
-        entfernung = new int[20][20];
-        bestRoute = (ArrayList<Stadt>) lst.clone();
-        ArrayList<Stadt> route = new ArrayList<Stadt>();
+        entfernungen = new int[20][20];
+        possibleRoutes = new ArrayList<>(lst);
+        ArrayList<Stadt> route = new ArrayList<>();
         bruteForceFindBestRoute(route, lst);
     }
 
-    public void bruteForceFindBestRoute
-        (ArrayList<Stadt> r,
-         ArrayList<Stadt> citiesNotInRoute)
-    {
-        if(!citiesNotInRoute.isEmpty())
-        {
-            for(int i = 0; i<citiesNotInRoute.size(); i++)
-            {
-                Stadt justRemoved =
-                    (Stadt) citiesNotInRoute.remove(0);
-                ArrayList<Stadt> newRoute =
-                    (ArrayList<Stadt>) r.clone();
+    private void bruteForceFindBestRoute(ArrayList<Stadt> route, ArrayList<Stadt> citiesNotInRoute) {
+        if (!citiesNotInRoute.isEmpty()) {
+            for (int i = 0; i < citiesNotInRoute.size(); i++) {
+                Stadt justRemoved = citiesNotInRoute.remove(0);
+                ArrayList<Stadt> newRoute = new ArrayList<>(route);
                 newRoute.add(justRemoved);
-                
+
                 bruteForceFindBestRoute(newRoute, citiesNotInRoute);
-                 citiesNotInRoute.add(justRemoved);
+                citiesNotInRoute.add(justRemoved);
             }
-        }
-        else
-        {
-            
-            bestRoute = r;
+        } else {
+
+            possibleRoutes = route;
             entfernungsliste();
-            isBestRoute(r);
+            getShortestRoute(route);
         }
-       
-        
     }
-    
-     private int getRandomInt(){
-        Random rand = new Random();
-        int randomPos = rand.nextInt(800);
-        return randomPos;
-    }
-    
-    private boolean isBestRoute(ArrayList<Stadt> r) {
+
+    private void getShortestRoute(ArrayList<Stadt> route) {
         int laenge = 0;
-        for( int i= 0; i < anzahl; i++){
-            if(i < anzahl-1)
-            laenge += entfernung[i][i+1];
-            else if(i == anzahl-1)
-            laenge += entfernung[i][0];
+        for (int i = 0; i < anzahl; i++) {
+            if (i < anzahl - 1)
+                laenge += entfernungen[i][i + 1];
+            else if (i == anzahl - 1)
+                laenge += entfernungen[i][0];
         }
-        if (laenge < gesamtlaenge){
+        if (laenge < gesamtlaenge) {
             gesamtlaenge = laenge;
-            shortestRoute = (ArrayList<Stadt>) r.clone();
+            shortestRoute = new ArrayList<>(route);
             naechsteStadt();
-        } 
-        return true;
+        }
     }
-    
-    public int entfernung(int s1, int s2){
-       int dx, dy, dx2, dy2;
-        dx = bestRoute.get(s1).xPos - bestRoute.get(s2).xPos;
-        dy = bestRoute.get(s1).yPos - bestRoute.get(s2).yPos;
-         dx2 = dx * dx;
-         dy2 = dy * dy;
-       
-         return (int)Math.sqrt(dx2 + dy2);
+
+    private int entfernung(int s1, int s2) {
+        int dx, dy, dx2, dy2;
+        dx = possibleRoutes.get(s1).xPos - possibleRoutes.get(s2).xPos;
+        dy = possibleRoutes.get(s1).yPos - possibleRoutes.get(s2).yPos;
+        dx2 = dx * dx;
+        dy2 = dy * dy;
+
+        return (int) Math.sqrt(dx2 + dy2);
     }
-    
-    public void entfernungsliste(){
-         
-        for(int k = 0; k < anzahl; k++){
-            
-            for(int i = 0; i < anzahl; i++)
-            {
-             entfernung[i][k] = entfernung(i,k);
+
+    private void entfernungsliste() {
+
+        for (int k = 0; k < anzahl; k++) {
+
+            for (int i = 0; i < anzahl; i++) {
+                entfernungen[i][k] = entfernung(i, k);
             }
         }
-        
+
     }
-    
-    public void naechsteStadt(){
-        for(int i = 0; i < anzahl;i++){
-            if(i < anzahl-1)
-            shortestRoute.get(i).next = i+1;
-            else 
-            shortestRoute.get(i).next = 0;
+
+    private void naechsteStadt() {
+        for (int i = 0; i < anzahl; i++) {
+            if (i < anzahl - 1)
+                shortestRoute.get(i).next = i + 1;
+            else
+                shortestRoute.get(i).next = 0;
         }
-        
+
     }
-    
-    public void paint(Graphics g)
-    {
+
+    void paint(Graphics g) {
         int next = 0;
-        int x1,y1,x2,y2;
-        
-        do{
+        int x1, y1, x2, y2;
+
+        do {
             shortestRoute.get(next).paint(g);
             x1 = shortestRoute.get(next).xPos;
             y1 = shortestRoute.get(next).yPos;
             next = shortestRoute.get(next).next;
             x2 = shortestRoute.get(next).xPos;
             y2 = shortestRoute.get(next).yPos;
-            g.drawLine(x1,y1,x2,y2);
+            g.drawLine(x1, y1, x2, y2);
         }
         while (next != 0);
     }
